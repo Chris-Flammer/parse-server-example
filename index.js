7989af7c6d636ca01abb718ae17ed708f2a4fde1
Chris Flammer <chrisflammer@gmail.com>
2016-04-12 17:11:52 -0600

    Change Configuration Values

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'DcFqlEmTakYkP83j0EFt',
+  masterKey: process.env.MASTER_KEY || 'e2ESY2FzTJgLTw6zssrN', //Add your master key here. Keep it secret!
+  clientKey: process.env.CLIENT_Key || 'SrlwdGfbJp9eYv8eAZjY' // Client Keys aren't really needed.  This is just for whatevs
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
